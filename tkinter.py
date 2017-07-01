b6d40894ca3e4f42d08f85a9c3153a8aab5a252a
Steven Copeland <steven@thomasleecopeland.com>
2017-07-01 15:12:15 -0400

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,17 +1,3 @@
-"""from tkinter import *
-main_window = Tk()
-count_label = Label(main_window, text="Count: 0")
-count_label.grid(row=0, column=1)
-count_value = 0
-def increment_count():
-    global count_value, count_label
-    count_value = count_value + 1
-    count_label.configure(text="Count: " + str(count_value))
-incr_button = Button(main_window, text="Increment", command=increment_count)
-incr_button.grid(row=0, column=0)
-quit_button = Button(main_window, text="Quit", background="red", command=main_window.destroy)
-quit_button.grid(row=1, column=0)"""
-
 from tkinter import *
 from random import *
 from sys import *
