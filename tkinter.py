678cd8157e92093211e3a6ddabc63a0ead6376ab
Steven Copeland <steven@thomasleecopeland.com>
2017-07-02 15:03:22 -0400

    Changing button size
    Changing the size of the button.

--- tkinter.py
+++ tkinter.py
@@ -26,29 +26,17 @@ def card():
     card4 = Button(program, text="card", command=maunderFunc)
     card4.grid(row=3, column=3)
 
-fulton = Button(program, text="Fulton", command=fultonFunc, padx=25, pady=25)
+fulton = Button(program, text="Fulton 0", command=fultonFunc, padx=25, pady=30)
 fulton.grid(row=0, column=0)
-# fulton number
-fulton = Button(program, text="0", command=fultonFunc, padx=25, pady=25)
-fulton.grid(row=0, column=1)
 
-algor = Button(program, text="Algor", command=algorFunc, padx=25, pady=25)
+algor = Button(program, text="Algor 0", command=algorFunc, padx=27, pady=30)
 algor.grid(row=0, column=3)
-# algor number
-algor = Button(program, text="0", command=algorFunc, padx=25, pady=25)
-algor.grid(row=0, column=4)
 
-kurseddane = Button(program, text="Kurseddane", command=kurseddaneFunc, padx=25, pady=25)
+kurseddane = Button(program, text="Kurseddane 0", command=kurseddaneFunc, padx=10, pady=30)
 kurseddane.grid(row=2, column=0)
-# kurseddande number
-kurseddane = Button(program, text="0", command=kurseddaneFunc, padx=25, pady=25)
-kurseddane.grid(row=2, column=1)
 
-maunder = Button(program, text="Maunder", command=maunderFunc, padx=25, pady=25)
+maunder = Button(program, text="Maunder 0", command=maunderFunc, padx=16, pady=30)
 maunder.grid(row=2, column=3)
-# maunder number
-maunder = Button(program, text="0", command=maunderFunc, padx=25, pady=25)
-maunder.grid(row=2, column=4)
 
 card1 = Button(program, text="card", command=kurseddaneFunc, padx=25, pady=25)
 card1.grid(row=3, column=0)
