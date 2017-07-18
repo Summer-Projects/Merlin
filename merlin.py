983a2dce95030b25a1fe34b4af5265de652cfb8b
GoRedbirds <johnowen338@gmail.com>
2017-07-18 10:29:07 -0400

    Update merlin.py

--- merlin.py
+++ merlin.py
@@ -1,12 +1,44 @@
-"""Game MERLIN"""
+"""Game MERLIN with TKinter"""
+#imports modules and makes variables
+from tkinter import *
 from random import random
 kurseddane, algor, maunder, fullton = 0,0,0,0
 counties = ['FULLTON','ALGOR','MAUNDER','KURSEDDANE']
 cards = []
 hand = []
-showHand = []
 AIhand = []
 x = 0
+wildCard = {}
+started = False
+kDict = {'python': kurseddane, 'tkinter': kurseddaneTK}
+fDict = {'python': fullton, 'tkinter': fulltonTK}
+mDict = {'python': maunder, 'tkinter': maunderTK}
+aDict = {'python': algor, 'tkinter': algorTK}
+
+#makes tkinter program
+program = Tk()
+text = Label(program, text="Pick one of your cards")
+text.grid(row=5, column=1, columnspan=2)
+quitGame = Button(program, text="Quit", command=program.destroy, padx=25, pady=25)
+quitGame.grid(row=6, column=6)
+fulltonTK = Button(program, text="Fullton: " + str(fullton), padx=25, pady=30)
+fulltonTK.grid(row=0, column=1)
+algorTK = Button(program, text="Algor: " + str(algor), padx=27, pady=30)
+algorTK.grid(row=0, column=2)
+kurseddaneTK = Button(program, text="Kurseddane: " + str(kurseddane), padx=10, pady=30)
+kurseddaneTK.grid(row=2, column=1)
+maunderTK = Button(program, text="Maunder: " + str(maunder), padx=16, pady=30)
+maunderTK.grid(row=2, column=2)
+card0 = Button(program, text=hand[0]['fullname'], command=card0func, padx=25, pady=25,width=23)
+card0.grid(row=4, column=0)
+card1 = Button(program, text=hand[1]['fullname'], command=card1func, padx=25, pady=25,width=23)
+card1.grid(row=4, column=1)
+card2 = Button(program, text=hand[2]['fullname'], command=card2func, padx=25, pady=25,width=23)
+card2.grid(row=4, column=2)
+card3 = Button(program, text=hand[3]['fullname'], command=card3func, padx=25, pady=25,width=23)
+card3.grid(row=4, column=3)
+
+#python functions
 def makeCard(n1,n2,w):
     x = {'name1': n1, 'name2':n2, 'fullname': n1 + " " + n2, 'worth': w}
     cards.append(x)
@@ -30,102 +62,153 @@ def cutDeck():
                 del(cards[position])
 
 def addCardsToHand():
-    global cards, hand, showHand
+    global cards, hand
     if len(hand) < 4:
         draw = int(len(cards) * random())
         hand.append(cards[draw])
-        showHand.append(cards[draw]['fullname'])
         del(cards[draw])
         addCardsToHand()
 
-def chooseCard():
-    global showHand, x
-    joinedHand = ', '.join(showHand)
-    ask = input('Your hand is ' + joinedHand + '. Which card do you pick?').upper()
-    if not ask in showHand:
-        print("Sorry, that is not a card in your hand")
-        chooseCard()
+#card functions
+def card0func():
+    global maunder, kurseddane, algor, fullton, wildCard, card0
+    if not hand[0]['name1'] == 'WILD':
+        army = hand[0]['worth']
+        county = hand[0]['name1']
+        if county == 'KURSEDDANE':
+            kurseddane += army
+        elif county == 'ALGOR':
+            algor += army
+        elif county == 'MAUNDER':
+            maunder += army
+        elif county == 'FULLTON':
+            fullton += army
+        del(hand[0])
+        main()
     else:
-        x = 0
-        while x < 4:
-            if showHand[x] == ask:
-                break
-            x += 1
-        del(showHand[x])
-    return x
-def pickCounty(n):
-    global hand
-    if hand[n]['name1'] == 'WILD':
-        ask = input('You picked a wild card. Which county do you want?').upper()
-        if not ask in counties:
-            print("Sorry, that is not a county")
-            pickCounty(n)
-        return ask
+        wildCard = hand[0]
+        del(hand[0])
+        wild()
+
+def card1func():
+    global maunder, kurseddane, algor, fullton, wildCard, card1
+    if not hand[1]['name1'] == 'WILD':
+        army = hand[1]['worth']
+        county = hand[1]['name1']
+        if county == 'KURSEDDANE':
+            kurseddane += army
+        elif county == 'ALGOR':
+            algor += army
+        elif county == 'MAUNDER':
+            maunder += army
+        elif county == 'FULLTON':
+            fullton += army
+        del(hand[1])
+        main()
     else:
-        return hand[n]['name1']
+        wildCard = hand[1]
+        del(hand[1])
+        wild()
 
-def war(army, county):
-    global maunder, kurseddane, algor, fullton
-    if county == 'KURSEDDANE':
-        kurseddane += army
-    elif county == 'ALGOR':
-        algor += army
-    elif county == 'MAUNDER':
-        maunder += army
-    elif county == 'FULLTON':
-        fullton += army
-
-def aiHand():
-    if len(AIhand) < 4:
-        draw = int(random()*len(cards))
-        AIhand.append(cards[draw])
-        del(cards[draw])
-        aiHand()
-
-def aiWar(cardNum):
-    global maunder, kurseddane, algor, fullton
-    card = AIhand[cardNum]
-    if card['name1'] == 'WILD':
-        choose = int(random()*4)
-        county = counties[choose]
+def card2func():
+    global maunder, kurseddane, algor, fullton, wildCard,card2
+    if not hand[2]['name1'] == 'WILD':
+        army = hand[2]['worth']
+        county = hand[2]['name1']
+        if county == 'KURSEDDANE':
+            kurseddane += army
+        elif county == 'ALGOR':
+            algor += army
+        elif county == 'MAUNDER':
+            maunder += army
+        elif county == 'FULLTON':
+            fullton += army
+        del(hand[2])
+        main()
     else:
-        county = card['name1']
-    worth = card['worth']
-    if county == 'KURSEDDANE':
-        kurseddane -= worth
-    elif county == 'ALGOR':
-        algor -= worth
-    elif county == 'MAUNDER':
-        maunder -= worth
-    elif county == 'FULLTON':
-        fullton -= worth
-    print('King Arthur Intellect played a ' + card['name2'] + ' against ' + county)
-    del(AIhand[cardNum])
+        wildCard = hand[2]
+        del(hand[2])
+        wild()
+
+def card3func():
+    global maunder, kurseddane, algor, fullton, wildCard,card3
+    if not hand[3]['name1'] == 'WILD':
+        army = hand[3]['worth']
+        county = hand[3]['name1']
+        if county == 'KURSEDDANE':
+            kurseddane += army
+        elif county == 'ALGOR':
+            algor += army
+        elif county == 'MAUNDER':
+            maunder += army
+        elif county == 'FULLTON':
+            fullton += army
+        del(hand[3])
+        main()
+    else:
+        wildCard = hand[3]
+        del(hand[3])
+        wild()
+
+#wild functions
+def wild():
+    fulltonTK.configure(command=fulltonWild)
+    algorTK.configure(command=algorWild)
+    maunderTK.configure(command=maunderWild)
+    kurseddaneTK.configure(command=kurseddaneWild)
+    text.configure(text='Pick a county for a wild card')
     
+def fulltonWild():
+    global fullton,wildCard
+    army = wildCard['worth']
+    fullton += army
+    wildCard = {}
+    main()
+def algorWild():
+    global algor,wildCard
+    army = wildCard['worth']
+    algor += army
+    wildCard = {}
+    main()
+def maunderWild():
+    global maunder,wildCard
+    army = wildCard['worth']
+    maunder += army
+    wildCard = {}
+    main()
+def kurseddaneWild():
+    global kurseddane, wildCard
+    army = wildCard['worth']
+    kurseddane += army
+    wildCard = {}
+    main()
+#tkinter functins
+def refresh():
+    text.configure(text='Pick one of your cards')
+    card0.configure(text=hand[0]['fullname'])
+    card1.configure(text=hand[1]['fullname'])
+    card2.configure(text=hand[2]['fullname'])
+    card3.configure(text=hand[3]['fullname'])
+    fulltonTK.configure(text='Fullton: ' + str(fullton))
+    algorTK.configure(text='Algor: ' + str(algor))
+    maunderTK.configure(text='Maunder: ' + str(maunder))
+    kurseddaneTK.configure(text='Kurseddane: ' + str(kurseddane))
+
+def backgrounds(county):
+    if county['python'] > 0:
+        county['tkinter'].configure(background='red')
+    elif county['python'] < 0:
+        county['tkinter'].configure(background='blue')
+    else:
+        county['tkinter'].configure(background='white')
+
 def main():
     cutDeck()
     addCardsToHand()
-    placement = chooseCard()
-    power = hand[placement]['worth']
-    county = pickCounty(placement)
-    del(hand[placement])
-    war(power, county)
-    aiHand()
-    pick = int(random()*4)
-    aiWar(pick)
-    nameToNumDict = {'FULLTON': fullton, 'KURSEDDANE': kurseddane, 'MAUNDER': maunder, 'ALGOR': algor}
-    for field in nameToNumDict:
-        if nameToNumDict[field] > 0:
-            print('You have a force of ' + str(nameToNumDict[field]) + ' in ' + field)
-        elif nameToNumDict[field] < 0:
-            print('King Arthur Intellect has a force of ' + str(nameToNumDict[field] * -1) + ' in ' + field)
-        else:
-            print('No one has a force in ' + field)
+    refresh()
+    countiesList = [kDict,mDict,fDict,aDict]
+    for x in countiesList:
+        backgrounds(x)
     if kurseddane > 0 and maunder > 0 and fullton > 0 and algor > 0:
-        print('Congrats! You have defeated King Arthur Intellect! You are now Lord of Merlin')
-    elif kurseddane < 0 and maunder < 0 and fullton< 0 and algor < 0:
-        print('King Arthur Intellect has defeated you, and is now Lord of Merlin. You go back to your country in shame')
-    else:
-        main()
-        
+        text.configure(text="Congrats! You have defeated King Arthur Intellect and are Lord of Merlin!")
 main()
